2ab6deef33fd9a59e6522764ed7b0f53d22f8717
Clément <jean_c@git.epitech.eu>
2015-05-15 14:05:49 +0200

    Map

--- src/MapGenerator.cpp
+++ src/MapGenerator.cpp
@@ -5,43 +5,43 @@
 // Login   <jean_c@epitech.net>
 //
 // Started on  Fri May 15 13:34:23 2015 clément jean
-// Last update Fri May 15 13:58:03 2015 clément jean
+// Last update Fri May 15 14:01:46 2015 clément jean
 //
 
 #include "MapGenerator.hh"
 
 MapGenerator::MapGenerator(const int &width, const int &height)
 {
-  this->_m_width = width;
-  this->_m_height = height;
-  this->_m_maze.resize(this->_m_width * this->_m_height);
+  this->_width = width;
+  this->_height = height;
+  this->_maze.resize(this->_width * this->_height);
 }
 
 void	MapGenerator::Generate()
 {
   this->Initialize();
   this->Carve(2, 2);
-  this->_m_maze[this->_m_width + 2] = true;
-  this->_m_maze[(this->_m_height - 2) *
-	       this->_m_width + this->_m_width - 3] = true;
+  this->_maze[this->_width + 2] = true;
+  this->_maze[(this->_height - 2) *
+	       this->_width + this->_width - 3] = true;
 }
 
 void	MapGenerator::Initialize()
 {
-  std::fill(this->_m_maze.begin(), this->_m_maze.end(), false);
-  for(int x = 0; x < this->_m_width; x++) {
-    this->_m_maze[x] = true;
-    this->_m_maze[(this->_m_height - 1) * this->_m_width + x] = true;
+  std::fill(this->_maze.begin(), this->_maze.end(), false);
+  for(int x = 0; x < this->_width; x++) {
+    this->_maze[x] = true;
+    this->_maze[(this->_height - 1) * this->_width + x] = true;
   }
-  for(int y = 0; y < this->_m_height; y++) {
-    this->_m_maze[y * this->_m_width] = true;
-    this->_m_maze[y * this->_m_width + this->_m_width - 1] = true;
+  for(int y = 0; y < this->_height; y++) {
+    this->_maze[y * this->_width] = true;
+    this->_maze[y * this->_width + this->_width - 1] = true;
   }
 }
 
 void MapGenerator::Carve(const int &x, const int &y)
 {
-  this->_m_maze[y * this->_m_width + x] = true;
+  this->_maze[y * this->_width + x] = true;
   int d = std::rand();
   int dirs[] = { 1, -1, 0, 0 };
 
@@ -50,9 +50,9 @@ void MapGenerator::Carve(const int &x, const int &y)
     int dy = dirs[(i + d + 2) % 4];
     int x1 = x + dx, y1 = y + dy;
     int x2 = x1 + dx, y2 = y1 + dy;
-    if(!this->_m_maze[y1 * this->_m_width + x1]
-       && !this->_m_maze[y2 * this->_m_width + x2]) {
-      this->_m_maze[y1 * this->_m_width + x1] = true;
+    if(!this->_maze[y1 * this->_width + x1]
+       && !this->_maze[y2 * this->_width + x2]) {
+      this->_maze[y1 * this->_width + x1] = true;
       Carve(x2, y2);
     }
   }
@@ -60,7 +60,7 @@ void MapGenerator::Carve(const int &x, const int &y)
 
 const std::vector<bool>     &MapGenerator::getMap()
 {
-  return this->_m_maze;
+  return this->_maze;
 }
 
 MapGenerator::~MapGenerator()
