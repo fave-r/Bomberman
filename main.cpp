f164d15ebd70bbc04aa62262ba10ec945c688d7f
soueuls <swann.polydor@epitech.eu>
2015-05-15 14:02:22 +0200

    Mockup main

---
+++ main.cpp
@@ -0,0 +1,28 @@
+//
+// main.cpp for Bomberman in /home/polydo_s/rendu/Bomberman
+//
+// Made by polydo_s
+// Login   <polydo_s@epitech.net>
+//
+// Started on  Fri May 15 13:51:26 2015 polydo_s
+// Last update Fri May 15 13:54:57 2015 polydo_s
+//
+
+#include <iostream>
+
+#include "MapGenerator.hh"
+
+int	main(int ac, char *av[])
+{
+  if (ac != 3)
+    {
+      std::cerr << "You need to pass a width and height for the map" << std::endl;
+      return (1);
+    }
+  else
+    {
+      MapGenerator *map = new MapGenerator(std::stoi(av[1]), std::stoi(av[2]));
+      map->Show();
+    }
+  return (0);
+}
