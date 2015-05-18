a3ce4af4c03a6cff74e7b6dd85174ba82c1f9f16
Arnaud Lhommels <arnaud.lhomme@epitech.eu>
2015-05-18 15:18:52 +0200

    genmap

---
+++ includes/GenMap.hh
@@ -0,0 +1,47 @@
+//
+// GenMap.hh for GenMap in /home/lhomme_a/rendu/Bomberman
+// 
+// Made by Arnaud Lhomme
+// Login   <lhomme_a@epitech.net>
+// 
+// Started on  Tue May  5 15:00:01 2015 Arnaud Lhomme
+// Last update Mon May 18 14:46:12 2015 Arnaud Lhomme
+//
+
+#ifndef GENMAP_HH_
+#define GENMAP_HH_
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <time.h>
+
+class	GenMap
+{
+private:
+  int					size_x;
+  int					size_y;
+  int					players;
+  std::vector<std::vector<char> >	map;
+  std::list <std::pair<int, int> >	drillers;
+
+public:
+  GenMap(int x, int y, int players);
+  ~GenMap();
+
+  int	getX() const;
+  void	setX(int);
+
+  int	getY() const;
+  void	setY(int);
+
+  int		getPlayers() const;
+  void		setPlayers(int);
+
+  std::vector<std::vector <char> >	getMap() const;
+
+  void	generate();
+};
+
+#endif
