f8ae361f559ed4a898a02040d85d93dc94872710
Clément <jean_c@git.epitech.eu>
2015-05-19 20:14:36 +0200

    hpp to cpp

---
+++ includes/Cube.hh
@@ -0,0 +1,40 @@
+//
+// Cube.hpp for Cube.hpp in /home/jean_c/Graphic
+//
+// Made by clément jean
+// Login   <jean_c@epitech.net>
+//
+// Started on  Tue May  5 19:29:04 2015 clément jean
+// Last update Tue May 19 20:12:24 2015 clément jean
+//
+
+#ifndef __CUBE_HPP__
+# define __CUBE_HPP__
+
+#include <iostream>
+#include <Geometry.hh>
+#include <Texture.hh>
+#include "AObject.hpp"
+
+class Cube : public AObject
+{
+public:
+  Cube();
+  virtual ~Cube();
+  const gdl::Texture	&getTexture() const;
+  const gdl::Geometry	&getGeometry() const;
+  void			setGeometry(const gdl::Geometry &old);
+  void			setTexture(const gdl::Texture &old);
+  const bool		&newTexture();
+  virtual bool		initialize();
+  void			move(const int &x, const int &y, const int &z);
+  virtual void		update(const gdl::Clock &clock);
+  virtual void		draw(const gdl::AShader &shader);
+
+private:
+  gdl::Geometry _geometry;
+  float		_speed;
+  gdl::Texture	_texture;
+};
+
+#endif
