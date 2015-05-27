773f4dc5126dcabb9648ecf764ffadeab6bb7c4a
Clément <jean_c@git.epitech.eu>
2015-05-27 11:24:01 +0200

    Textures

--- src/TexturePool.cpp
+++ src/TexturePool.cpp
@@ -4,7 +4,7 @@
 // Login   <jean_c@epitech.net>
 //
 // Started on  Mon May 25 02:15:39 2015 clément jean
-// Last update Wed May 27 02:02:46 2015 clément jean
+// Last update Wed May 27 11:22:22 2015 clément jean
 //
 
 #include "TexturePool.hh"
@@ -34,7 +34,7 @@ bool		TexturePool::init()
       std::cerr << "Cannot load the player texture" << std::endl;
       return (false);
     }
-  if (this->_textureGround.load("./Assets/ground.fbm/Grass_tile_B_diffuse.tga") == false)
+  if (this->_textureGround.load("./Assets/ground.fbm/ground.tga") == false)
     {
       std::cerr << "Cannot load the ground texture" << std::endl;
       return (false);
